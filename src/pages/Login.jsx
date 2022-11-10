1f997da97f4c3b221230756b669b9c3292ee45ba
pnichols789 <pnichols789@gmail.com>
2022-11-10 13:13:58 -0600

    -

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Container, Form } from "react-bootstrap";
+
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+	const [email, setEmail] = useState("");
 
-export default Login
+	return (
+		<div className="login" style={{backgroundImage: 'linear-gradient(rgba(0,0,0,.3), rgba(0,0,0,.3)), url('../assets/login-bg.jpg')'}}>
+            <h2>Login</h2>
+			<Container>
+				<Form>
+					<Form.Control
+						placeholder="Email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
+				</Form>
+			</Container>
+		</div>
+	);
+};
+
+export default Login;
