b48e6bb5f88f295fcab8b8a442f2d8bbe7118ece
pnichols789 <pnichols789@gmail.com>
2022-11-10 10:22:22 -0600

    -

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,41 +1,61 @@
-import React, { useState } from 'react'
-import { Container, Nav, Navbar } from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
-
+import React, { useState } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navigation = () => {
-    const [isAuth, setIsAuth] = useState(true)
-    const navigate = useNavigate()
+	const [isAuth, setIsAuth] = useState(true);
+	const navigate = useNavigate();
 
-    const handleLogout = () => {
-        setIsAuth(false)
-    }
-    const handleLogin = () => {
-        setIsAuth(true)
-    }
-  return (
-    <Navbar bg="light" expand="lg">
-      <Container>
-        <Link to='' className='nav-brand'>Penny Blog</Link>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            { isAuth ? (
-                <>
-                    <Link className='nav-link' to='/profile' >Profile</Link>
-                    <Link className='nav-link' to='/new-blog' >New</Link>
-                    <Link className='nav-link' to='/login' onClick={handleLogout}>Logout</Link>
-                </>
+	const handleLogout = () => {
+		setIsAuth(false);
+	};
+	const handleLogin = () => {
+		setIsAuth(true);
+		navigate("/login");
+	};
+	return (
+		<Navbar bg="light" expand="lg">
+			<Container>
+				<Link to="" className="nav-brand">
+					<img
+						className="logo"
+						src="https://github.com/PennyNichols/blog-app/blob/master/src/assets/logo.png?raw=true"
+						alt="Penny Nichols"
+						style={{
+							height: "3.5rem",
+						}}
+					/>
+				</Link>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="ms-auto">
+						{isAuth ? (
+							<>
+								<Link className="nav-link" to="/profile">
+									Profile
+								</Link>
+								<Link className="nav-link" to="/new-blog">
+									New
+								</Link>
+								<Link className="nav-link" to="/login" onClick={handleLogout}>
+									Logout
+								</Link>
+							</>
+						) : (
+							<>
+								<Nav.Item className="nav-link" onClick={handleLogin}>
+									Login
+								</Nav.Item>
+								<Nav.Item className="nav-link" onClick={()=>navigate('/register')}>
+									Register
+								</Nav.Item>
+							</>
+						)}
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
 
-            ) : (
-               <Link className='nav-link' to='' onClick={handleLogin}>Login</Link> 
-            )}
-            
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  )
-}
-
-export default Navigation
+export default Navigation;
---
+++ src/helpers/firebase.js
@@ -0,0 +1,93 @@
+import { initializeApp } from "firebase/app";
+import {getDatabase} from 'firebase/database'
+import {
+	getAuth,
+	createUserWithEmailAndPassword,
+	updateProfile,
+	signInWithEmailAndPassword,
+	onAuthStateChanged,
+	signOut,
+	GoogleAuthProvider,
+	signInWithPopup,
+	sendPasswordResetEmail,
+} from "firebase/auth";
+import {getAnalytics} from 'firebase/analytics'
+
+
+// .env variable assignment setup:
+// REACT_APP_apiKey=
+// REACT_APP_authDomain=
+// REACT_APP_projectId=
+// REACT_APP_storageBucket=
+// REACT_APP_messagingSenderId=
+// REACT_APP_appId=
+
+
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messageSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.REACT_APP_measurementId
+};
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app)
+const analytics = getAnalytics(app)
+export const db = getDatabase(app)
+
+
+// Register a user 
+export const registerUser = async(email, password,displayName) => { 
+    try {
+  
+      await createUserWithEmailAndPassword(auth, email, password)
+      await updateProfile(auth.currentUser, {displayName})
+  
+    } catch(err) { 
+      return err.message.replace('Firebase:', '')
+    }
+  }
+  
+  // login
+  export const login = async(email, password) => { 
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+    } catch (err) { 
+      return err.message.replace("Firebase:", "");
+    }
+  }
+  
+  // user observer 
+  export const userObserver = (setCurrentUser) => { 
+    onAuthStateChanged(auth, (user) => { 
+      if (user) setCurrentUser(user)
+      else setCurrentUser(null)
+    })
+  }
+  
+  // signout 
+  export const logout = () => { 
+    signOut(auth)
+  }
+  
+  export const signUpProvider = async () => { 
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(auth, provider);
+  }
+  
+  export const forgetPassword = async (email) => { 
+    try {
+      await sendPasswordResetEmail(auth, email);
+      return 'Please check your mail box !'
+  
+    } catch (err) { 
+      console.log(err);
+      return err.message.replace("Firebase:", "");
+    }
+  }
