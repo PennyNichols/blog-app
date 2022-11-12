8ad75844a82eeb33e0262c2fa71d31e9494369a7
pnichols789 <pnichols789@gmail.com>
2022-11-11 20:12:45 -0600

    -

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { Card } from 'react-bootstrap'
 
-const BlogCard = () => {
+const BlogCard = (props) => {
+    console.log(props.blog)
+    const {imgUrl, title, body, author} = props.blog
   return (
-    <div>BlogCard</div>
+    <div className='rounded shadow my-2 p-2' style={{ width: '25rem', backgroundColor: "#d3d3d3e2"  }}>
+        <img classNames="d-block m-auto"  style={{width: '10rem'}} src={imgUrl} alt={title} />
+        <Card.Body>
+            <Card.Title>{title}</Card.Title>
+            <Card.Subtitle>{author}</Card.Subtitle>
+            <Card.Text>
+                {body.substring(0,150)}...
+            </Card.Text>
+        </Card.Body>
+        
+    </div>
   )
 }
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,30 @@
-import React, { useContext } from 'react'
-import { BlogContext } from '../contexts/BlogContext';
+import React, { useContext } from "react";
+import BlogCard from "../components/BlogCard";
+import { BlogContext } from "../contexts/BlogContext";
 
 const Dashboard = () => {
-    const {blogs } = useContext(BlogContext);
-    console.log(blogs)
-  return (
-    <div>Dashboard</div>
-  )
-}
+	const { blogs } = useContext(BlogContext);
+	console.log(blogs);
+	return (
+		<div style={{backgroundColor: '#606060'}}>
+			<div className="d-flex justify-content-center align-items-center">
+				<div
+					className="mx-2 mb-2"
+					style={{ height: "5px", width: "100px", backgroundColor: "white" }}
+				></div>
+				<h1 className="text-light">Dashboard</h1>
+				<div
+					className="mx-2 mb-2"
+					style={{ height: "5px", width: "100px", backgroundColor: "white" }}
+				></div>{" "}
+			</div>
+			<div className='d-flex flex-wrap p-3 gap-3'>
+				{blogs.map((blog, index) => {
+					return <BlogCard key={index} blog={blog} />;
+				})}
+			</div>
+		</div>
+	);
+};
 
-export default Dashboard
+export default Dashboard;
